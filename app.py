6b70e21e4dc61bbd62055551a5dc81ce0acfa6d0
Zoniacer <63639703+Zoniacer@users.noreply.github.com>
2022-12-21 10:51:14 +0700

    app deployed just need the right model

--- app.py
+++ app.py
@@ -1,68 +1,43 @@
-import pandas as pd
-from flask import Flask, request, render_template
-from keras.models import Model
-import tensorflow as tf
+from flask import Flask, render_template, request
+from keras.models import load_model
+import cv2
+import matplotlib.pyplot as plt 
+import numpy as np
 
-app = Flask("__name__")
+app = Flask(__name__)
+
+dic = {0 : 'Ayam Bakar', 1 : 'Bakso', 2 : 'Gado gado', 3 : 'Rendang', 4 : 'Sate'}
+
+model = load_model('cnn_adam_categorical_crossentropy')
+
+def predict_label(img_path):
+   img = cv2.imread(img_path)
+#    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB),cmap=plt.cm.binary)
+   img = cv2.resize(img,(198,259))
+   img = np.expand_dims(img, axis=0)
+   pred = model.predict(img)
+   prediction  = np.argmax(pred, axis=1)
+   return dic[int(prediction)], pred
 
 
-@app.route("/")
-def loadPage():
-	return render_template('home.html', query="")
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	return render_template("home.html")
+
+@app.route("/submit", methods = ['GET', 'POST'])
+def get_output():
+	if request.method == 'POST':
+		img = request.files['my_image']
+
+		img_path = "static/" + img.filename	
+		img.save(img_path)
+
+		p,pred = predict_label(img_path)
+
+	return render_template("home.html", prediction = p, img_path = img_path,pred=pred)
 
 
-@app.route("/", methods=['POST'])
-def predict():
-    
-    
-    inputQuery1 = request.form['query1']
-    inputQuery2 = request.form['query2']
-    inputQuery3 = request.form['query3']
-    inputQuery4 = request.form['query4']
-    inputQuery5 = request.form['query5']
-    inputQuery6 = request.form['query6']
-    inputQuery7 = request.form['query7']
-    inputQuery8 = request.form['query8']
-    inputQuery9 = request.form['query9']
-    inputQuery10 = request.form['query10']
-    inputQuery11 = request.form['query11']
-    inputQuery12 = request.form['query12']
-
-    
-    cnn_model = tf.keras.models.load_model('cnn_model')
-    
-    data = [[float(inputQuery1), float(inputQuery2), float(inputQuery3), float(inputQuery4), 
-             float(inputQuery5), float(inputQuery6), float(inputQuery7), float(inputQuery8),
-             float(inputQuery9), float(inputQuery10), float(inputQuery11), float(inputQuery12)]]
-    
-    df = pd.DataFrame(data, 
-                      columns = [ 'credit_score', 'gender', 'age', 'tenure', 'balance', 'products_number',
-                                            'credit_card', 'active_member', 'estimated_salary',
-                                            'country_France', 'country_Germany', 'country_Spain'])
-    
-    
-    #final_df=pd.concat([new_df__dummies, new_dummy], axis=1)
-  
-    probablity = cnn_model.predict(data)
-    if probablity>=0.5:
-        o1 = "This customer is likely to be churned!!"
-        o2 = "Confidence: {}".format(probablity*100)
-    else:
-        o1 = "This customer is likely to continue!!"
-        o2 = "Confidence: {}".format(probablity*100)
-        
-    return render_template('home.html', output1=o1, output2=o2, 
-                           query1 = request.form['query1'], 
-                           query2 = request.form['query2'],
-                           query3 = request.form['query3'],
-                           query4 = request.form['query4'],
-                           query5 = request.form['query5'], 
-                           query6 = request.form['query6'], 
-                           query7 = request.form['query7'], 
-                           query8 = request.form['query8'], 
-                           query9 = request.form['query9'], 
-                           query10 = request.form['query10'], 
-                           query11 = request.form['query11'], 
-                           query12 = request.form['query12'])
-    
-app.run()
+if __name__ =='__main__':
+	#app.debug = True
+	app.run(debug = True)
